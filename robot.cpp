46d03a47ebe905f446d583b83ca4c5da0619f195
kabutakot <47484259+kabutakot@users.noreply.github.com>
2020-02-21 11:57:06 +0300

    Add files via upload

---
+++ robot.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+class Info{
+	char name[20];
+	int raw, prod, money, plants, auto_plants;
+public:
+	void info_name(char iname[20]);
+	void info_raw(int iraw);
+	void info_prod(int iprod);
+	void info_plants(int iplants);
+	void info_auto(int iauto);
+	void get_name(
+	 
